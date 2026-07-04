84d7e978dc7d04aa5f54506eae063a58302ad140
Hồ Nguyễn Đăng Khôi <96038162+honguyendangkhoi@users.noreply.github.com>
2026-07-04 09:16:34 +0700

    Create env.js

---
+++ Extension/env.js
@@ -0,0 +1,19 @@
+const COGNITO_CONFIG = {
+  userPoolId: add_your_userPool,
+  domain: add_your_domain,
+  appClientId: add_your_appClientId,
+  region: add_your_region
+};
+
+const GOOGLE_CONFIG = {
+  clientId: add_your_clientId
+};
+
+const MICROSOFT_CONFIG = {
+    clientId: add_your_clientId
+};
+
+const API_CONFIG = {
+    baseUrl: add_your_baseUrl,
+    syncListEndpoint: add_your_syncListEndpoint
+};
